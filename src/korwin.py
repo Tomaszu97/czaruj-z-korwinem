08f4737c08db0ac61fa4d6791cee5ee2f5864ae0
Tomasz Jakubowski <tomaszjakubowski97@gmail.com>
2018-11-10 18:22:51 +0100

    poprawione sciezki do plikow w kodzie

--- src/korwin.py
+++ src/korwin.py
@@ -11,19 +11,19 @@ from kivy.clock import Clock
 from kivy.core.audio import SoundLoader
 
 class MainController(PageLayout):
-	atak_cyborga = SoundLoader.load('data/atak_cyborga.mp3')
-	awaria_cybernetycznej_krtani = SoundLoader.load('data/awaria_cybernetycznej_krtani.mp3')
-	informacja_dla_reptilianskich_zwierzchnikow = SoundLoader.load('data/informacja_dla_reptilianskich_zwierzchnikow.mp3')
-	ladowanie_ataku_obszarowego = SoundLoader.load('data/ladowanie_ataku_obszarowego.mp3')
-	ladowanie_mocy = SoundLoader.load('data/ladowanie_mocy.mp3')
-	odblokowanie_5_czary = SoundLoader.load('data/odblokowanie_5_czary.mp3')
-	okrzyk_plemienny = SoundLoader.load('data/okrzyk_plemienny.mp3')
-	przemiana_w_jaszczura = SoundLoader.load('data/przemiana_w_jaszczura.mp3')
-	przywolanie_bractwa_reptoidow = SoundLoader.load('data/przywolanie_bractwa_reptoidow.mp3')
-	zaklecie_4_stron_swiata = SoundLoader.load('data/zaklecie_4_stron_swiata.mp3')
-	zaklecie_7_kregu_magii = SoundLoader.load('data/zaklecie_7_kregu_magii.mp3')
-	zaklecie_niewybaczalne = SoundLoader.load('data/zaklecie_niewybaczalne.mp3')
-	yellow_submarine = SoundLoader.load('data/yellow_submarine.mp3')
+	atak_cyborga = SoundLoader.load('../data/atak_cyborga.mp3')
+	awaria_cybernetycznej_krtani = SoundLoader.load('../data/awaria_cybernetycznej_krtani.mp3')
+	informacja_dla_reptilianskich_zwierzchnikow = SoundLoader.load('../data/informacja_dla_reptilianskich_zwierzchnikow.mp3')
+	ladowanie_ataku_obszarowego = SoundLoader.load('../data/ladowanie_ataku_obszarowego.mp3')
+	ladowanie_mocy = SoundLoader.load('../data/ladowanie_mocy.mp3')
+	odblokowanie_5_czary = SoundLoader.load('../data/odblokowanie_5_czary.mp3')
+	okrzyk_plemienny = SoundLoader.load('../data/okrzyk_plemienny.mp3')
+	przemiana_w_jaszczura = SoundLoader.load('../data/przemiana_w_jaszczura.mp3')
+	przywolanie_bractwa_reptoidow = SoundLoader.load('../data/przywolanie_bractwa_reptoidow.mp3')
+	zaklecie_4_stron_swiata = SoundLoader.load('../data/zaklecie_4_stron_swiata.mp3')
+	zaklecie_7_kregu_magii = SoundLoader.load('../data/zaklecie_7_kregu_magii.mp3')
+	zaklecie_niewybaczalne = SoundLoader.load('../data/zaklecie_niewybaczalne.mp3')
+	yellow_submarine = SoundLoader.load('../data/yellow_submarine.mp3')
 
 class KorwinApp(App):
 	def build(self):
